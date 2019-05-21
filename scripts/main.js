68c2dc890502cb0b2b95a2284d67723eb478be17
Naranja-Sagged <alexliuying@gmail.com>
2019-05-21 00:56:02 -0700

    events: resize/click, swapTab; Add epic gifs

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,18 @@
 var imgTogglers = document.getElementsByClassName("toggleImg");
 var otherImages = document.getElementsByClassName("images");
+var tabSwitchers = document.getElementsByClassName("switchTab");
+var curTab = document.getElementById("pics");
 
 for (var i = 0; i < imgTogglers.length; ++i) {
     imgTogglers[i].addEventListener("click", toggleImg);
 }
 
+for (var i = 0; i < tabSwitchers.length; ++i) {
+    tabSwitchers[i].addEventListener("click", switchTab);
+}
+
 window.addEventListener("load", resizeImgs);
+window.addEventListener("resize", resizeImgs);
 
 function toggleImg() {
     var picId = this.attributes["linked-img"].value;
@@ -25,5 +32,16 @@ function resizeImgs() {
         for (var j = 0; j < lis.length; ++ j) {
             lis[j].style.height = lis[j].offsetWidth + "px";
         }
+    }
+}
+
+function switchTab() {
+    var tabId = this.attributes["linked-tab"].value;
+    var tab = document.getElementById(tabId);
+    if (tab != curTab) {
+        curTab.className = "hidden images";
+        tab.className = "images";
+        curTab = tab;
+        resizeImgs();
     }
 }
